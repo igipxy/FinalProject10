6882e3e0a66d72c13230703a06282a68a079e6ca
iGipxy <gesangrah@gmail.com>
2025-09-30 09:17:39 +0700

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,11 +1,18 @@
+import java.util.Scanner;
+
 public class Test {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
 
         System.out.println("omg guys pls collaborate");
         System.out.println("Gesang doesnt help anything");
         
             System.out.println("Gesang Ngising 1 hari:  ");
-            double GesangNgising ;
+            sc = next.double;
+            double GesangNgising;
+
+            
             
     }
     
