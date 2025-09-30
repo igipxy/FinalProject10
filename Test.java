2d1e1e4813f00474614904b2b036c5eb2d42998e
AthaHalilintar <Atharasya088@gmail.com>
2025-09-30 09:04:40 +0700

    aku nambahin sout

--- Test.java
+++ Test.java
@@ -2,6 +2,7 @@ public class Test {
     public static void main(String[] args) {
 
         System.out.println("omg guys pls collaborate");
+        System.out.println("Gesang doesnt help anything");
     }
     
 }
