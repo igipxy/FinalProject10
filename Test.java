a269e3316fac16cb692be103d58a4b6dbdf6876e
iGipxy <gesangrah@gmail.com>
2025-09-30 11:30:22 +0700

    change almost everything cuh very unorganized

--- Test.java
+++ Test.java
@@ -4,12 +4,9 @@ public class Test {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-
-        System.out.println("omg guys pls collaborate");
-        System.out.println("Gesang doesnt help anything");
-            
-        
-        System.out.println("Gesang Makan per hari:  ");
+        System.out.println("who are you? ");
+        String input = sc.nextLine();
+        System.out.println("You entered: " + input);
 
         sc.close();
 
